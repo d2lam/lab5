d45f462db375cd316387991804742c59e4bede49
Dao Lam <d2lam@ucsd.edu>
2014-02-06 14:58:45 -0800

    added stuff

--- routes/add.js
+++ routes/add.js
@@ -2,11 +2,12 @@ var data = require('../data.json');
 
 exports.view = function(req, res) {    
 	// Your code goes here
-	res.render('add');
+	
 	var newFriend = {
-		name: req.query.name,
-		description: req.query.description
+		'name': req.query.name,
+		'description': req.query.description
 	}
+	res.render('add',newFriend);
 	data["friends"].push(newFriend);
 
  }
